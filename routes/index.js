620a29b62c09d442aceb4ba122a335d4cf694d27
dvdfl <flo22001@byui.edu>
2023-03-13 21:51:40 -0400

    seetings endpoint added

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
+router.use('/settings', require('./settings'))
 
 module.exports = router;
