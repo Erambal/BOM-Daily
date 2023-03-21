1584db6b7a4584e472bb13af8b24fd8dfde3fb80
Aaron <97133493+AaronGusa@users.noreply.github.com>
2023-03-21 09:59:26 -0600

    Update user.js
    Revert to earlier version to support controller

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const user = require('../controllers/user');
+//Req's
+const router = require('express').Router();
+const useControl = require('../controllers/user');
 
-// router.get('/:array', user.getUsers);
-router.get('/:id', user.getUser);
-router.post('/', user.createUser);
-router.put('/:id', user.updateUser);
-// router.delete('/:array', user.deleteUsers);
-router.delete('/:id', user.deleteUser);
+//routes
+//-------------------------------------- user
+router.get('/', useControl.getAllUsers)
+router.post('/', useControl.postUser);
+router.put('/', useControl.putUser);
 
+//-------------------------------------- userId
+router.get('/:id', useControl.getUserId);
+router.put('/:id', useControl.putUser);
+router.delete('/:id', useControl.deleteUserId);
+
+//-------------------------------------- userName
+router.get('/userprofile/:username', useControl.getUsername);
+router.put('/userprofile/:username', useControl.putUsername);
+router.delete('/userprofile/:username', useControl.deleteUsername);
+
+//-------------------------------------- imageUpload
+router.post('/userprofile/:id/uploadImage', useControl.putUser);
+
+
+//-------------------------------------- login - OAuth
+
+
+//exports
 module.exports = router;
+
