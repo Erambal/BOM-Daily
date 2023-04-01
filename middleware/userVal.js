f1e9def2d9b40bcb248d6c0645b30b56bffe854a
Goosa2025 <gus21002@byui.edu>
2023-04-01 02:08:19 -0600

    OAuth and validator for user added. Ready for test

---
+++ middleware/userVal.js
@@ -0,0 +1,32 @@
+const validator = require('../helper/validateUser');
+const valUser = async (req, res, next) => {
+    const validationRule = {
+        "username": "required|string",
+        "fname": "required|string",
+        "lname": "required|string",
+        "street": "required|string",
+        "city": "required|string",
+        "state": "required|string",
+        "zipcode": "required|string",
+        "email": "required|email",
+        "phone": "required|string",
+        "img_url": "required|string",
+        "admin":"boolean"
+    };
+
+    await validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+            res.status(412)
+                .send({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+        } else {
+            next();
+        }
+    }).catch( err => console.log(err))
+}
+module.exports = {
+    valUser
+};
