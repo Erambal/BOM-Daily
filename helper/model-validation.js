b722ea6b6f3dabb167b4a29e5945f2b5052eafef
dvdfl <flo22001@byui.edu>
2023-04-04 23:47:55 -0400

    model validation middleware

---
+++ helper/model-validation.js
@@ -0,0 +1,23 @@
+const Validator = require('validatorjs');
+const validator = async (body, rules, customMessages, callback) => {
+    const validation = new Validator(body, rules, customMessages);
+    validation.passes(() => callback(null, true));
+    validation.fails(() => callback(validation.errors, false));
+};
+const validateModel = async (req, res, next, validationRule) =>{
+
+    await validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+            res.status(412)
+                .send({
+                    success: false,
+                    message: 'Validation failed',
+                    data: err
+                });
+        } else {
+            next();
+        }
+    }).catch( err => next(err))
+
+}
+module.exports = validateModel;
--- routes/scripture.js
+++ routes/scripture.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const scripture = require('../controllers/scripture');
+const val = require('../middleware/scripture-val');
 
 router.get('/', scripture.getScriptures);
+
 router.get('/:id', scripture.getScriptureById);
-router.post('/', scripture.createScripture);
-router.put('/:id', scripture.updateScripture);
-// router.delete('/:array', scripture.deleteScriptures);
+
+router.post('/', val.validateScripture, scripture.createScripture);
+
+router.put('/:id', val.validateScripture, scripture.updateScripture);
+
 router.delete('/:id', scripture.deleteScripture);
 
 module.exports = router;
